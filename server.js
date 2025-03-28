510bec157e146221480da9c5cd58af9c4b41f7f7
areyes2853 <46231827+areyes2853@users.noreply.github.com>
2025-03-28 16:18:34 -0400

    connection MongoDB cars

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-// Here is where we import modules
-// We begin by loading Express
-const express = require('express');
+// server.js
+
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+// Connect to MongoDB using the connection string in the .env file
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  });
+
 app.listen(3001, () => {
-  console.log('Listening on port 3001');
+  console.log("Listening on port 3001");
 });
+
 
 
 //server.js
 //get /
 app.get("/", async (req,res)=>{
-    res.send("hello, friend!")
+    res.render("index.ejs")
 });
