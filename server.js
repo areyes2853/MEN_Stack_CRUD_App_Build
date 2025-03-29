f5babe0913a65ec16bf878a92388bd79297d7051
areyes2853 <46231827+areyes2853@users.noreply.github.com>
2025-03-28 20:54:14 -0400

    fixing small issues

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override"); // new
+const morgan = require("morgan");
 
 const app = express();
 
@@ -17,6 +19,8 @@ mongoose.connection.on("connected", () => {
 const carModel = require("./models/cars.js");
 //lest us accept form
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method")); // new
+app.use(morgan("dev")); //new
 
 
 app.listen(3001, () => {
@@ -42,12 +46,56 @@ app.post("/cars", async (req, res) => {
         req.body.isCarReadytoBeSold=false;}
     // res.send(req.body)
     await carModel.create(req.body)
-    res.redirect("/cars/new");
+    res.redirect("/cars");
   });
   
 
 // GET /fruits/new
 app.get("/cars/new", (req, res) => {
     res.render("cars/new.ejs");
+  });
+  
+  
+  app.get("/cars/:carId", async (req, res) => {
+    const foundCars = await carModel.findById(req.params.carId)
+    res.render("cars/show.ejs",{cars: foundCars});
+  });
+  
+  
+  app.get("/cars",async (req,res)=>{
+    const allCars = await carModel.find();
+    console.log(allCars)
+    res.render("cars/index.ejs",{cars:allCars})
+    // res.send("welcome to the index page!")
+
+  })
+
+  app.delete("/cars/:carId", async (req, res) => {
+    await carModel.findByIdAndDelete(req.params.carId);
+    res.redirect("/cars");
+  });
+  
+  // GET localhost:3000/fruits/:fruitId/edit
+app.get("/cars/:carId/edit", async (req, res) => {
+    const foundCar = await carModel.findById(req.params.carId);
+    console.log(foundCar);
+    res.render("cars/edit.ejs", {
+        car: foundCar,
+      });
+  });
+  
+
+  app.put("/cars/:carId", async (req, res) => {
+    if(req.body.isCarReadytoBeSold){
+        req.body.isCarReadytoBeSold = true;
+    }
+    else{
+        req.body.isCarReadytoBeSold =false
+    }
+     // Update the car in the database
+  await carModel.findByIdAndUpdate(req.params.carId, req.body);
+
+  // Redirect to the car's show page to see the updates
+  res.redirect(`/cars/${req.params.carId}`);
   });
   
